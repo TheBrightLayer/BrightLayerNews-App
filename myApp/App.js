df5a36b86caaf07ad3efdafc76af879103f4cc99
AksVerse <akshitverma296@gmail.com>
2025-09-26 20:04:29 +0530

    "Basic setup for the 'app' with tailwindcss"

---
+++ myApp/App.js
@@ -0,0 +1,12 @@
+import "nativewind";
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-blue-100">
+      <Text className="text-xl font-bold text-blue-700">
+        Expo Web + Tailwind ✅
+      </Text>
+    </View>
+  );
+}
